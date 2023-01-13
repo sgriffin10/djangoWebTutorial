4077fe4f37625b64d698adc68d5978e73eb2de1d
sgriffin10 <sgriffin4@babson.edu>
2023-01-13 11:52:46 -0500

    add landing, login, logout, signup routes/pages

--- website/__init__.py
+++ website/__init__.py
@@ -8,6 +8,17 @@ def create_app():
     app = Flask(__name__) #__name__ is name of file that you ran
     app.config['SECRET_KEY'] = 'random string'
 
+    #need to tell init.py about our routes
+
+    from .views import views
+    from .auth import auth
+
+    '''
+    define these as empty routes because anything inside views and auth files starts with the url_prefix (es) define below 
+    '''
+    app.register_blueprint(views,url_prefix='/') 
+    app.register_blueprint(auth,url_prefix='/')
+
     return app
-    
+
     
--- website/views.py
+++ website/views.py
@@ -1,7 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint #means
 '''
 defines url endpoints for front end of site
+define that this file is a blueprint for our app
+don't need to define all routes in one file
 '''
 
+views = Blueprint('views',__name__)
 
-
+@views.route('/') #decorator
+def home(): #route
+    return "<h1>Test</h1>"
