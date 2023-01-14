3699b5bf3c40d7b2dd52bc2f37618df0f1ff9505
sgriffin10 <sgriffin4@babson.edu>
2023-01-14 15:11:07 -0500

    add note adding & deleting functionality

--- website/views.py
+++ website/views.py
@@ -1,5 +1,9 @@
-from flask import Blueprint,render_template
+from flask import Blueprint,render_template,request,flash,jsonify
 from flask_login import login_required,current_user #can access anything from user model as current user if user is logged in
+from . import models
+from . import db
+import json
+
 '''
 defines url endpoints for front end of site
 define that this file is a blueprint for our app
@@ -8,7 +12,28 @@ don't need to define all routes in one file
 
 views = Blueprint('views',__name__)
 
-@views.route('/') #decorator
+@views.route('/',methods=['GET','POST']) #decorator
 @login_required
 def home(): #route
+    if request.method == 'POST':
+        note = request.form.get('note')
+        if len(note) < 1:
+            flash('Note is too short', category='error')
+        else:
+            new_note = models.Note(data=note,user_id=current_user.id)
+            db.session.add(new_note)
+            db.session.commit()
+            flash('Note added', category='success')
+
     return render_template("home.html", user=current_user)
+
+@views.route('/delete-note',methods=['POST'])
+def delete_note():
+    note = json.loads(request.data)
+    noteID = note['noteId']
+    note = models.Note.query.get(noteID)
+    if note:
+        if note.user_id == current_user.id:
+            db.session.delete(note)
+            db.session.commit()
+    return jsonify({})
