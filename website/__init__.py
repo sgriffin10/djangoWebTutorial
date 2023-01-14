6738ad39dfc37524f650fd20c20187f7e79febf9
sgriffin10 <sgriffin4@babson.edu>
2023-01-14 12:04:55 -0500

    define db model

--- website/__init__.py
+++ website/__init__.py
@@ -3,10 +3,16 @@ makes website folder a python package
 means that we can import website folder and run init.py file
 '''
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__) #__name__ is name of file that you ran
     app.config['SECRET_KEY'] = 'random string'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}' #stores db inside website folder
+    db.init_app(app) #tells db what app were gonna use
 
     #need to tell init.py about our routes
 
--- website/models.py
+++ website/models.py
@@ -1,3 +1,19 @@
 '''
 defines database models
 '''
+from . import db #importing db object
+from flask_login import UserMixin #custom class that we can inheret that can give user object specific things for flask login
+from sqlalchemy.sql import func
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True),default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    first_name = db.Column(db.String(150))
+    notes = db.relationship('Note') #capitalized; adds each note to user_id 
